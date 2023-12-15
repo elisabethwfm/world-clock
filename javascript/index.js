12c81732bf585ba6082fafc36f7689f0ef42923a
elisabethwfm <elisabeth.wolfram@gmail.com>
2023-12-15 12:56:27 +0100

    correct timezone for london

--- javascript/index.js
+++ javascript/index.js
@@ -3,10 +3,10 @@ let londonElement = document.querySelector("#london");
 let londonDate = londonElement.querySelector(".date");
 let londonTime = londonElement.querySelector(".time");
 
-londonDate.innerHTML = `<div class="date">${moment().format(
-  "MMMM Do YYYY"
-)}</div>`;
+londonDate.innerHTML = `<div class="date">${moment()
+  .tz("Europe/London")
+  .format("MMMM Do YYYY")}</div>`;
 
-londonTime.innerHTML = `<div class="time">${moment().format(
-  "hh:mm:ss"
-)} <section>${moment().format("A")}</section></div>`;
+londonTime.innerHTML = `<div class="time">${moment()
+  .tz("Europe/London")
+  .format("hh:mm:ss")} <section>${moment().format("A")}</section></div>`;
