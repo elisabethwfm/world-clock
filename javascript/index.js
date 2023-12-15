c7ca40a061595d5c314a9cf64af7923e5e0f5e80
elisabethwfm <elisabeth.wolfram@gmail.com>
2023-12-15 12:29:28 +0100

    one approach for the query selector

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,6 @@
-let londonTime = document.querySelector("#london");
+let londonTime = document.querySelector("#london .time");
+let londonDate = document.querySelector("#london .date");
 
 londonTime.innerHTML = "Happy coding!";
+
+londonDate.innerHTML = "Happy coding!";
